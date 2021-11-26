a04fdd2de87682ac464917506136d7ee3f600295
Williams OZSERTTAS <wiozsert@e3r2p23.clusters.42paris.fr>
2021-11-26 14:28:24 +0100

    End

--- srcs/mandatory/init_get_data/init_and_get_command.c
+++ srcs/mandatory/init_get_data/init_and_get_command.c
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 14:38:05 by wiozsert          #+#    #+#             */
-/*   Updated: 2021/11/26 14:16:40 by wiozsert         ###   ########.fr       */
+/*   Updated: 2021/11/26 14:26:01 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ t_data	*init_and_get_cmd(t_data *data, char **av, int count, int i)
 	data->cmd = NULL;
 	data->cmd = (char **)malloc(sizeof(char *) * (count + 1));
 	data->cmd_nbr = count;
-	printf("%d\n", data->cmd_nbr);
 	if (data->cmd == NULL)
 		data_cmd_malloc_failed(data);
 	data->cmd[count] = NULL;
